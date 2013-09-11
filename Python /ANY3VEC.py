018e490a86cf46e938e12b9ecb2a425c4f67a3a1
Ethan McCue <emccue@live.com>
2013-09-11 17:21:59 -0400

    fixed a stupid syntax error

--- Python /ANY3VEC.py
+++ Python /ANY3VEC.py
@@ -1,6 +1,6 @@
 import random
 #'THIS MODULE PRODUCES THREE RANDOM VECTORS.  THE ANGLES ARE POLAR.
-def main()
+def main():
     vv1 = int((random.random() * 900))
     vv2 = int((random.random() * 1000))
     vv3 = int((random.random() * 500) + 100)
@@ -12,7 +12,7 @@ def main()
     print("Vector #1 has a magnitude of " + str(vv2) + " and a direction of " + str(aa2) + " degrees polar.")
     print("Vector #1 has a magnitude of " + str(vv3) + " and a direction of " + str(aa3) + " degrees polar.")
 
-    print()
+   
     
 if __name__ == "__main__":
     main()
