d8d47948249c0483672e7e1265c4b2a514e49101
Ethan McCue <emccue@live.com>
2013-09-11 15:02:36 -0400

    Added replacement for DATA.BAS

---
+++ BASIC_qb/Programs/DATA.py
@@ -0,0 +1,45 @@
+#REM ***Marty Thornley***
+#REM ***Period 3***
+#REM ***4/14/93***
+#REM ***Read & Data***
+#' This program uses the READ and DATA statements to             
+#' read the dimensions of a box and calculate its volume.        
+
+#' Major variables:                                              
+#'       Length    length of the box                             
+#'       Wdth      width of the box                              
+#'       Height    height of the box                             
+#'       Volume    volume of the box                             
+#CLS
+#READ Length, Wdth, Height
+#Volume = Length * Wdth * Height
+#PRINT "Volume of the box is "; Volume
+#DATA 2.75, 4.5, 8.2
+#END
+
+#  ^ original program
+
+#Credit goes to Marty Thornley for this program
+from sys import platform as _platform
+import os
+if _platform == "linux" or _platform == "linux2":
+    clearscreen = "os.system('clear')"
+    # linux
+elif _platform == "darwin":
+    clearscreen = "os.system('clear')"
+    # OS X
+elif _platform == "win32":
+    clearscreen = "os.system('cls')"
+
+def main():
+    print("Enter the Length")
+    length = input()
+    print("Enter the Width")
+    width = input()
+    print("Enter the Height")
+    height = input()
+    volume = length * width * height
+    print("Volume of the box is " + str(volume))
+
+if __name__=="__main__":
+    main()
